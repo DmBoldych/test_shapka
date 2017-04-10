a4f6f34acb5214b5bbc638c6bde5d856b86e4c4b
DmBoldych <dm.boldych@gmail.com>
2017-04-10 15:07:50 +0300

    Header test Bootstrap

---
+++ js/common.js
@@ -0,0 +1,28 @@
+$(function() {
+
+	var i = 0;
+    $('.img_like').click(function(){
+        i++;
+        $('.likeit').html(i);
+    });
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
